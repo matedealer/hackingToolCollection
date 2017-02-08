a44646ff046c0f6f1789a5ec8a9c29bcbb089e98
matedealer <git@matedealer.de>
2017-02-08 18:34:06 +0100

    rearranged

--- other_skripts/listUpdates.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-foo=$(sudo pacman -Sy 2> /dev/null)
-count=$(pacman -Qu | wc -l)
-printf "== Updatable Packages ======\n"
-printf "There are "$count" Packages to update\n"
-printf "============================\n\n"
